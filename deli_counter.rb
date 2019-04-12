0e0e9bfbb22262e58dba088c8aec41b9af7a9b53
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-12 18:54:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli) 
+ puts "The line is currently empty." if katz_deli.empty?
+ if !katz_deli.empty?
+    message = "The line is currently:"
+   katz_deli.each_with_index do |val,index|
+     message += " #{index.to_i+1}. #{val.strip}"
+   end 
+    puts "#{message}"
+ end
+end 
+
+def take_a_number(katz_deli,name)
+  puts "Welcome, #{name}. You are number 1 in line." if katz_deli.empty?
+end
