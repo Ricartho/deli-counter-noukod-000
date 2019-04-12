68c81d3c550f3beef23e4b876f550bc3a61fb193
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-12 19:26:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,5 +26,5 @@ def now_serving(katz_deli)
   puts "There is nobody waiting to be served!" if katz_deli.empty?
   puts  "Currently serving #{katz_deli[0]}." if !katz_deli.empty?
       katz_deli.shift
-  end 
+  
 end 
